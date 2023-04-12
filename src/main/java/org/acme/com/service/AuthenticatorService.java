dbc8307c1d3fbae328e2736bbb913a0cb632481b
Thiago Oliveira <tjlemoso@gmail.com>
2023-04-12 16:51:53 -0300

    ajustes deploy

--- src/main/java/org/acme/com/service/AuthenticatorService.java
+++ src/main/java/org/acme/com/service/AuthenticatorService.java
@@ -22,29 +22,35 @@ public class AuthenticatorService {
 
     public AuthenticatorResponse validadeUser(String name, String password){
 
-        UserDTOResponse user = userService.getUserByName(name);
+        try {
+                       
+            UserDTOResponse user = userService.getUserByName(name);
 
+            if(user.getPassword().equals(password)){
+    
+                LOGGER.info("password equal");
+    
+                UserDTOResponse userResponse = new UserDTOResponse();
+                userResponse.setUserId(user.getUserId());
+                userResponse.setIsAdmin(user.getIsAdmin());
+                userResponse.setName(user.getName());
+                userResponse.setPassword(user.getPassword());
+                
+                AuthenticatorResponse response = new AuthenticatorResponse();
+                response.setUser(userResponse);
+                response.setToken("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlivFI8qB4D0y2jy0CfEqFyy46R0o7S8TKpsx5xbHKoU1VWg6QkQm+ntyIv1p4kE1sPEQO73+HY8+Bzs75XwRTYL1BmR1w8J5hmjVWjc6R2BTBGAYRPFRhor3kpM6ni2SPmNNhurEAHw7TaqszP5eUF/F9+KEBWkwVta+PZ37bwqSE4sCb1soZFrVz/UT/LF4tYpuVYt3YbqToZ3pZOZ9AX2o1GCG3xwOjkc4x0W7ezbQZdC9iftPxVHR8irOijJRRjcPDtA6vPKpzLl6CyYnsIYPd99ltwxTHjr3npfv/3Lw50bAkbT4HeLFxTx4flEoZLKO/g0bAoV2uqBhkA9xnQIDAQAB");
+                response.setAuthenticated(true);
+    
+                return response;
+    
+            } 
 
-        if(user.getPassword().equals(password)){
-
-            LOGGER.info("password equal");
-
-            UserDTOResponse userResponse = new UserDTOResponse();
-            userResponse.setUserId(user.getUserId());
-            userResponse.setIsAdmin(user.getIsAdmin());
-            userResponse.setName(user.getName());
-            userResponse.setPassword(user.getPassword());
+            return new AuthenticatorResponse();
             
-            AuthenticatorResponse response = new AuthenticatorResponse();
-            response.setUser(userResponse);
-            response.setToken("MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlivFI8qB4D0y2jy0CfEqFyy46R0o7S8TKpsx5xbHKoU1VWg6QkQm+ntyIv1p4kE1sPEQO73+HY8+Bzs75XwRTYL1BmR1w8J5hmjVWjc6R2BTBGAYRPFRhor3kpM6ni2SPmNNhurEAHw7TaqszP5eUF/F9+KEBWkwVta+PZ37bwqSE4sCb1soZFrVz/UT/LF4tYpuVYt3YbqToZ3pZOZ9AX2o1GCG3xwOjkc4x0W7ezbQZdC9iftPxVHR8irOijJRRjcPDtA6vPKpzLl6CyYnsIYPd99ltwxTHjr3npfv/3Lw50bAkbT4HeLFxTx4flEoZLKO/g0bAoV2uqBhkA9xnQIDAQAB");
-            response.setAuthenticated(true);
+        } catch (Exception e) {
+            LOGGER.info("Error {}", e.getMessage());
+            return new AuthenticatorResponse();
+        }        
 
-            return response;
-
-        } 
-        AuthenticatorResponse response = new AuthenticatorResponse();
-        //response.setAuthenticated(true);
-        return response ;
     }
 }
